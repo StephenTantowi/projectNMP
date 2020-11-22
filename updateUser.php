8616009071b3d86fa06a76c7caf3ecb58ba3c974
Tanjiro04 <tanjiro04@users.noreply.github.com>
2020-11-22 14:51:18 +0700

    Update updateUser.php

--- updateUser.php
+++ updateUser.php
@@ -10,19 +10,33 @@
 	}
 
 
-if($_POST['email'] && $_POST['password'] && $_POST['nama'] && $_POST['iduser']) {
+	if($_POST['password'] && $_POST['nama'] && $_POST["iduser"]) 
+	{
 		$iduser = $_POST['iduser'];
-		$email = $_POST['email'];
 		$password = $_POST['password'];
 		$nama = $_POST['nama'];
-		$sql = "UPDATE user SET nama = '$nama', email = '$email', password = '$password' WHERE iduser = $iduser";
+		$sql = "UPDATE user SET nama = '$nama', password = '$password' WHERE iduser = $iduser";
 		$con->query($sql);
 
 		$arr = array("result" => "OK", 
 			"sql"	=> $sql,
 			"message" => "user updated");
 		echo json_encode($arr);
-	} else {
+	} 
+	elseif ($_POST['nama'] && $_POST["iduser"]) 
+	{
+		$iduser = $_POST['iduser'];
+		$nama = $_POST['nama'];
+		$sql = "UPDATE user SET nama = '$nama' WHERE iduser = $iduser";
+		$con->query($sql);
+
+		$arr = array("result" => "OK", 
+			"sql"	=> $sql,
+			"message" => "user updated");
+		echo json_encode($arr);
+	}
+	else
+	{
 		$arr = array("result" => "ERROR", 
 			"message" => "data is empty");
 		echo json_encode($arr);

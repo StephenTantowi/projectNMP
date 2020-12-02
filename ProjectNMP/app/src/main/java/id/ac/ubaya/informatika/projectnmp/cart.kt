e7b85aa35c3730593db770978369ec8dd08e7afd
ViReNavy <71750098+ViReNavy@users.noreply.github.com>
2020-12-02 19:34:01 +0700

    plusMinus

--- ProjectNMP/app/src/main/java/id/ac/ubaya/informatika/projectnmp/cart.kt
+++ ProjectNMP/app/src/main/java/id/ac/ubaya/informatika/projectnmp/cart.kt
@@ -1,3 +1,3 @@
 package id.ac.ubaya.informatika.projectnmp
 
-data class cart(val iduser:Int, val idproduct:Int, val jumlah:Int, val harga:Int, val gambar:String, val nama:String)
+data class cart(val iduser:Int, val idproduct:Int, var jumlah:Int, var harga:Int, var gambar:String, var nama:String)
--- ProjectNMP/app/src/main/java/id/ac/ubaya/informatika/projectnmp/cartAdapter.kt
+++ ProjectNMP/app/src/main/java/id/ac/ubaya/informatika/projectnmp/cartAdapter.kt
@@ -1,10 +1,13 @@
 package id.ac.ubaya.informatika.projectnmp
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.cart_layout.view.*
 
@@ -25,6 +28,53 @@ class cartAdapter(val carts:ArrayList<cart>,val ctx: Context): RecyclerView.Adap
         holder.v.txtNamaP.text = carts[position].nama
         holder.v.txtHarga.text = carts[position].harga.toString()
         holder.v.txtQuanatity.text = carts[position].jumlah.toString()
+
+        holder.v.btnMinus.setOnClickListener {
+            var q = Volley.newRequestQueue(ctx)
+            val url = "http://ubaya.prototipe.net/nmp160418024/updateKeranjangSementara.php"
+            var stringRequest = object: StringRequest(com.android.volley.Request.Method.POST, url,
+                {
+                    Log.d("update",it)
+                    carts[position].jumlah--
+                    holder.v.txtQuanatity.text = carts[position].jumlah.toString()
+                },
+                {
+                    Log.d("update",it.message.toString())
+                }
+            ){
+                override  fun  getParams(): MutableMap<String,String>{
+                    var params = HashMap<String,String>()
+                    params.put("iduser",Global.users[0].id.toString())
+                    params.put("idproduct",carts[position].idproduct.toString())
+                    params.put("aktivitas","Kurang")
+                    return  params
+                }
+            }
+            q.add(stringRequest)
+        }
+        holder.v.btnPlus.setOnClickListener {
+            var q = Volley.newRequestQueue(ctx)
+            val url = "http://ubaya.prototipe.net/nmp160418024/updateKeranjangSementara.php"
+            var stringRequest = object: StringRequest(com.android.volley.Request.Method.POST, url,
+                {
+                    Log.d("update",it)
+                    carts[position].jumlah++
+                    holder.v.txtQuanatity.text = carts[position].jumlah.toString()
+                },
+                {
+                    Log.d("update",it.message.toString())
+                }
+            ){
+                override  fun  getParams(): MutableMap<String,String>{
+                    var params = HashMap<String,String>()
+                    params.put("iduser",Global.users[0].id.toString())
+                    params.put("idproduct",carts[position].idproduct.toString())
+                    params.put("aktivitas","Tambah")
+                    return  params
+                }
+            }
+            q.add(stringRequest)
+        }
 
     }
 
